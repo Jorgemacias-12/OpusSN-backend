f73718b736d8a659d4dc6d387dbbd92ec0d3f4d4
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2024-04-07 17:02:17 -0600

    update(route): implement basic routing
    get, get with id as param and post

--- src/routes/UserRoute.ts
+++ src/routes/UserRoute.ts
@@ -1,7 +1,23 @@
 import { Router, type Request, type Response } from "express";
 
-export const userRoutes = Router()
+export const userRoutes = Router();
 
 userRoutes.get('/', (req: Request, res: Response) => {
   res.send("User router enabled");
-})
+});
+
+userRoutes.get('/:id', (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  res.send(`Fetching user ${id} data`);
+});
+
+userRoutes.post('/', (req: Request, res: Response) => {
+  
+  const response = {
+    ...req.body,
+    message: "User creation successfully"
+  }
+
+  res.json(response);
+});
