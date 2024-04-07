afe8896f87092308e1b4c4355050ac65a5c9df5e
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2024-04-07 17:14:05 -0600

    update(routes): User endpoint
    Add example validation

--- src/routes/UserRoute.ts
+++ src/routes/UserRoute.ts
@@ -1,6 +1,12 @@
 import { Router, type Request, type Response } from "express";
+import { body, validationResult, type ValidationChain } from "express-validator";
 
 export const userRoutes = Router();
+
+const createUserValidationChainExample: ValidationChain[] = [
+  body('email').notEmpty().isEmail().withMessage("You need to provide a valid Email"),
+
+]
 
 userRoutes.get('/', (req: Request, res: Response) => {
   res.send("User router enabled");
@@ -12,8 +18,12 @@ userRoutes.get('/:id', (req: Request, res: Response) => {
   res.send(`Fetching user ${id} data`);
 });
 
-userRoutes.post('/', (req: Request, res: Response) => {
+userRoutes.post('/', createUserValidationChainExample ,(req: Request, res: Response) => {
   
+  const errors = validationResult(req);
+
+  if (errors && !errors.isEmpty()) res.status(418).json(errors);
+
   const response = {
     ...req.body,
     message: "User creation successfully"
