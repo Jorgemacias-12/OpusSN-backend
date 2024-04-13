40d6e05d23d43115882cf41bf28bf36b2ca294e7
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2024-04-13 15:10:29 -0600

    add(utils): endpoints operation errors

---
+++ src/utils/validationErrors.ts
@@ -0,0 +1,14 @@
+export const createUserErrorMessages = {
+  nameRequired: 'El nombre es requerido',
+  nameLength: "El tamaño el nombre debe ser entre 4 y 50 caracteres",
+  lastnameRequired: 'El apellido es requerido',
+  lastnameLength: 'El tamaño del apellido debe ser entre 5 y 30 caracteres',
+  emailRequired: 'El email es requerido',
+  emailLength: 'La longitud del mail no es válida',
+  emailIsNotValid: "El email no es válido",
+  usernameRequired: 'El nombre de usuario es requerido',
+  usernameLength: 'El nombre de usuario debe tener entre 3 y 15 caracteres',
+  usernameCharacters: 'El nombre de usuario solo puede contener letras, números , guiones y guiones bajos',
+  usernameAlreadyExists: 'El nombre de usuario se encuentra en uso :C',
+  passwordRequired: 'No puedes dejar el campo de contraseña vacio'
+}
