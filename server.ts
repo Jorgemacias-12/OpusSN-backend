63c787102306903f3a3c8f4324f79e4d4c1167fa
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2024-04-13 03:53:11 -0600

    update(server): include features
    Like setting up the following middlewares:
    
     - morgan in dev mode
     - cors
     - helmet
     - json
     - urlencoded
     - static (public)
    
    Then use the routers imported from /routes
    
     - /users
     - /categories
     - /posts
    
    and the last one listen to connections (bind)

--- server.ts
+++ server.ts
@@ -1,1 +1,39 @@
-console.log("Hello via Bun!");
+import cors from 'cors';
+import express, { type Express } from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import swaggerUi from 'swagger-ui-express';
+import { categoryRouter, postRouter, userRouter } from './src/routes';
+import swaggerDocument from './src/public/swagger.json'
+
+const PORT = process.env.PORT || 3000;
+const app: Express = express();
+
+// Middlewares
+app.use(morgan('dev'));
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("./src/public"));
+
+app.use('/users', userRouter);
+app.use('/categories', postRouter);
+app.use('/posts', categoryRouter);
+
+app.use(
+  "/docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument)
+)
+
+app.get('/', (req, res) => {
+  res.json({
+    message: "API deployment sucessfull"
+  });
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Express server start on ${PORT}`);
+});
