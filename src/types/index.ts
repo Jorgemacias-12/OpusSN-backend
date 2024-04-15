4c78bfed8fe9ff73a48f6fc54d976585d9005125
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2024-04-15 03:46:23 -0600

    update(types): index
    Add the following interface:
    
      - UserCreationResponse -> {
         message
         newUser
       }
    
       This interface is being used in the UsersController as return type in
       the createUser method.
    
    Add new enum values in the RESPONSE_CODES:
    
      - OK = 200
      - NOT_FOUND = 404
      - INTERNAL_SERVER_ERROR = 500

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,12 +1,14 @@
-import type { User, UserGetParams } from "../models/User";
+import type { NewUser, SafeUser } from "../models/User";
 
-export interface GetUsersResponse {
-  count: number;
-  users: Array<UserGetParams>
+export interface UserCreationResponse {
+  message: string;
+  newUser: SafeUser
 }
 
 export enum RESPONSE_CODES {
+  OK = 200,
   CREATED = 201,
   BAD_REQUEST = 400,
-  NOT_FOUND = 404
+  NOT_FOUND = 404,
+  INTERNAL_SERVER_ERROR = 500
 }
