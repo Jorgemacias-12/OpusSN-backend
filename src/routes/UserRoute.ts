be92c766ee39b3fd4a64c86dbe76700885378ad5
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2024-04-15 03:48:50 -0600

    update(routes): User Routes
    Comment all routes code due to refactor with return types and responses.
    
    The first refactor completed is POST operation to create a new user.
    
    In this REST Operation I've changed the createUserValidationChain to
    meet new user model strucure field naming.
    
    And copy & paste the same logic to updateUserValidationChain.

--- src/routes/UserRoute.ts
+++ src/routes/UserRoute.ts
@@ -1,9 +1,9 @@
-import { Router, type Request, type Response } from "express";
+import { Router, response, type Request, type Response } from "express";
 import { body, validationResult, type ValidationChain } from 'express-validator';
 import UserController from "../controllers/UsersController";
 import { createUserErrorMessages } from "../utils/validationErrors";
-import type { UserCreationParams } from "../models/User";
-import { RESPONSE_CODES } from "../types";
+import { RESPONSE_CODES, type UserCreationResponse } from "../types";
+import type { NewUser } from "../models/User";
 
 export const userRouter = Router();
 
@@ -11,46 +11,69 @@ const controller = new UserController();
 
 //!! Use controller to return the desired json data
 userRouter.get('/', async (req: Request, res: Response) => {
-  const result = await controller.getUsers();
+  // const { username, checkIfExists } = req.query;
 
-  const { users, count } = result!;
+  // if (checkIfExists == 'true') {
+  //   try {
+  //     const isUsernameAvailable = await controller.checkIfUsernameIsAvailable(username as string);
 
-  res.status(200).json({
-    usersCounter: count,
-    users
-  })
+  //     return res.status(RESPONSE_CODES.OK).json({
+  //       IsUsernameAvailable: isUsernameAvailable
+  //     });
+  //   }
+  //   catch (err) {
+  //     return res.status(RESPONSE_CODES.INTERNAL_SERVER_ERROR).json({
+  //       error: `Error checking for existing username info: ${err}`
+  //     });
+  //   }
+  // }
+
+  // try {
+  //   const result = await controller.getUsers();
+
+  //   const { users, count } = result!;
+
+  //   return res.status(RESPONSE_CODES.OK).json({
+  //     usersCount: count,
+  //     users
+  //   })
+  // }
+  // catch (err) {
+  //   return res.status(RESPONSE_CODES.INTERNAL_SERVER_ERROR).json({
+  //     error: `Error getting users info: ${err}`
+  //   });
+  // }
 });
 
 userRouter.get('/:id', async (req: Request, res: Response) => {
-  const { id } = req.params;
+  // const { id } = req.params;
 
-  const response = await controller.getUser(id);
+  // const response = await controller.getUser(id);
 
-  if (response === null) {
-    res.status(RESPONSE_CODES.NOT_FOUND).json({
-      message: `El usuario con id: ${id} no existe`
-    })
-  }
+  // if (response === null) {
+  //   res.status(RESPONSE_CODES.NOT_FOUND).json({
+  //     message: `El usuario con id: ${id} no existe`
+  //   })
+  // }
 
-  res.json(response);
-
+  // res.json(response);
 });
 
 
 const createUserValidationChain: ValidationChain[] = [
-  body('name')
+  body('Name')
     .notEmpty()
     .withMessage(createUserErrorMessages.nameRequired)
     .isLength({ min: 4, max: 50 })
     .withMessage(createUserErrorMessages.nameLength),
 
-  body('lastname')
+  body('LastName')
     .notEmpty()
     .withMessage(createUserErrorMessages.lastnameRequired)
     .isLength({ min: 5, max: 30 })
     .withMessage(createUserErrorMessages.lastnameLength),
 
-  body('email')
+  body('Email')
     .notEmpty()
     .withMessage(createUserErrorMessages.emailRequired)
     .isLength({ min: 5, max: 50 })
@@ -58,7 +81,7 @@ const createUserValidationChain: ValidationChain[] = [
     .isEmail()
     .withMessage(createUserErrorMessages.emailIsNotValid),
 
-  body('username')
+  body('UserName')
     .notEmpty()
     .withMessage(createUserErrorMessages.usernameRequired)
     .isLength({ min: 3, max: 15 })
@@ -73,49 +96,121 @@ const createUserValidationChain: ValidationChain[] = [
       }
     }),
 
-  body('password')
+  body('Password')
     .notEmpty()
     .withMessage(createUserErrorMessages.passwordRequired)
 ]
 
 userRouter.post('/', createUserValidationChain, async (req: Request, res: Response) => {
-  // Validate request body  
+  // Check for errors
   const errors = validationResult(req);
 
   if (errors != null && !errors.isEmpty()) {
-    return res.status(400).json(errors);
+    return res.status(RESPONSE_CODES.BAD_REQUEST).json(errors);
   }
 
-  const { name, lastname, username, email, password, role } = req.body;
+  // Get values from request body with object destruct
+  const { Name, LastName, UserName, Email, Password, Role } = req.body;
 
-  const user: UserCreationParams = {
-    name,
-    lastname,
-    username,
-    email,
-    password,
-    role
+  // Create NewUser type object
+  const newUser: NewUser = {
+    Name,
+    LastName,
+    UserName,
+    Email,
+    Password,
+    Role
   }
 
-  const result = await controller.createUser(user)
+  // Send the new user object to controller
+  // And get response object
+  const result: UserCreationResponse | null = await controller.createUser(newUser);
 
+  if (result === null) {
+    res.status(RESPONSE_CODES.INTERNAL_SERVER_ERROR).json({
+      error: "Internal Server Error - 500"
+    });
+
+    return;
+  }
+
+  // Send result object to client
   res.status(RESPONSE_CODES.CREATED).json(result);
 });
 
 // TODO: make const and define values
 let updateUserValidationChain: ValidationChain[] = [
+  body('Name')
+    .notEmpty()
+    .withMessage(createUserErrorMessages.nameRequired)
+    .isLength({ min: 4, max: 50 })
+    .withMessage(createUserErrorMessages.nameLength),
 
+  body('LastName')
+    .notEmpty()
+    .withMessage(createUserErrorMessages.lastnameRequired)
+    .isLength({ min: 5, max: 30 })
+    .withMessage(createUserErrorMessages.lastnameLength),
+
+  body('Email')
+    .notEmpty()
+    .withMessage(createUserErrorMessages.emailRequired)
+    .isLength({ min: 5, max: 50 })
+    .withMessage(createUserErrorMessages.emailLength)
+    .isEmail()
+    .withMessage(createUserErrorMessages.emailIsNotValid),
+
+  body('UserName')
+    .notEmpty()
+    .withMessage(createUserErrorMessages.usernameRequired)
+    .isLength({ min: 3, max: 15 })
+    .withMessage(createUserErrorMessages.usernameLength)
+    .matches(/^[a-zA-Z0-9_-]+$/)
+    .withMessage(createUserErrorMessages.usernameCharacters)
+    .custom(async (value) => {
+      let userExists: boolean = false;
+
+      if (userExists) {
+        throw new Error(createUserErrorMessages.usernameAlreadyExists);
+      }
+    }),
+
+  body('Password')
+    .notEmpty()
+    .withMessage(createUserErrorMessages.passwordRequired)
 ]
 
-userRouter.post('/:id', (req: Request, res: Response) => {
+userRouter.put('/:id', updateUserValidationChain, async (req: Request, res: Response) => {
+  // const { id } = req.params;
+  // const { name, lastname, username, email, password, role } = req.body;
 
+  // const user: UserCreationParams = {
+  //   name,
+  //   lastname,
+  //   username,
+  //   email,
+  //   password,
+  //   role
+  // }
+
+  // const result = await controller.updateUser(id, user)
+
+  // res.json(result);
 });
 
-// TODO: make const and define values
-let deleteUserValidationChain: ValidationChain[] = [
+userRouter.delete('/:id', async (req: Request, res: Response) => {
+  // const { id } = req.params;
 
-]
+  // const response = await controller.deleteUser(id);
 
-userRouter.delete('/:id', (req: Request, res: Response) => {
+  // if (response === null) {
+  //   res.status(RESPONSE_CODES.NOT_FOUND).json({
+  //     message: `El usuario con ${id}, no se ha podido eliminar`
+  //   })
+  // }
 
+  // res.json({
+  //   message: "User deleted sucessfully",
+  //   userDeletedData: response,
+  // });
 });
