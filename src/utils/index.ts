2daf08c95349c2b6c32e76369f4ca2e7edaab328
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2024-04-18 08:52:41 -0600

    update(utils): index
    Refactor to hashPassword function, to remove bcrypt lib, and use bun
    built in password encryption :P.
    
    Add function verifyPassword, using bun built in password.verify function
    to check hash and plain text password.

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,9 +1,11 @@
-import bcrypt from 'bcrypt';
-
 export const hashPassword = async (text: string) => {
-  const saltRounds = 12;
-
-  const hashedPassword = await bcrypt.hash(text, saltRounds);
+  const hashedPassword = await Bun.password.hash(text, {
+    algorithm: "bcrypt",
+  })
 
   return hashedPassword;
+}
+
+export const verifyPassword = async (plainPassword: string, hashedPassword: string) => {
+  return await Bun.password.verify(plainPassword, hashedPassword);
 }
