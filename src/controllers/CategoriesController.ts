65e8056e5912a4891d8706d23d5c6a72b6e8673d
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2024-04-24 17:03:35 -0600

    feat(CRUD): categories

--- src/controllers/CategoriesController.ts
+++ src/controllers/CategoriesController.ts
@@ -1,12 +1,220 @@
-import { Get, Route, SuccessResponse } from 'tsoa';
+import { Prisma, PrismaClient } from '@prisma/client';
+import { Body, Delete, Get, Path, Post, Route, SuccessResponse, Tags } from 'tsoa';
+import type { CategoryCollectionReponse, CategoryCreationResponse, CategoryDeletionResponse, CategoryResponse, CheckCategoryAvailabilityResponse } from '../types';
+import type { NewCategory } from '../models/Category';
 
 @Route("/categories")
+@Tags("Categories")
 export class CategoriesController {
+  prisma = new PrismaClient();
+
   @Get("/")
   @SuccessResponse("200", "Sucess")
-  public async getCategories(): Promise<{ message: string }> {
-    return {
-      message: ""
+  public async getCategories(): Promise<CategoryCollectionReponse> {
+    try {
+      const categories = await this.prisma.category.findMany();
+      const categoryCount = await this.prisma.category.count();
+
+      return {
+        categories,
+        categoryCount
+      }
+    }
+    catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        return {
+          categories: null,
+          categoryCount: 0,
+          error: {
+            code: error.code,
+            message: error.message,
+            meta: error.meta
+          }
+        }
+      }
+      else {
+        return {
+          categoryCount: 0,
+          categories: null,
+          error: {
+            code: "500",
+            message: "Something went wrong getting users",
+            meta: null
+          }
+        }
+      }
+    }
+    finally {
+      await this.prisma.$disconnect();
+    }
+  }
+
+  @Get("/:id")
+  public async getCategory(id: number): Promise<CategoryResponse> {
+    try {
+      const category = await this.prisma.category.findUnique({
+        where: {
+          id
+        }
+      });
+
+      if (category === null) {
+        return {
+          category: null,
+          error: {
+            message: `Couldn't find user with id ${id}`
+          }
+        }
+      }
+      
+      return {
+        category
+      }
+    }
+    catch (error) {
+      return {
+        category: null,
+        error: {
+          message: `Error trying to get category with id ${id}}`
+        }
+      }
+    }
+    finally {
+      await this.prisma.$disconnect();
+    }
+  }
+
+  @Post("/")
+  public async createCategory(@Body() category: NewCategory): Promise<CategoryCreationResponse> {
+    try {
+      const createdCategory = await this.prisma.category.create({
+        data: category
+      });
+
+      if (createdCategory === null) {
+        return {
+          message: null,
+          category: null,
+          error: {
+            message: "Could not create new category",
+            code: "500",
+            meta: null
+          }
+        }
+      }
+
+      return {
+        message: "¡Category created sucessfully!",
+        category: createdCategory
+      }
+    }
+    catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        return {
+          message: null,
+          category: null,
+          error: {
+            code: error.code,
+            message: error.message,
+            meta: error.meta
+          }
+        };
+      } else {
+        return {
+          message: `Error creating new category -> Internal server error}`,
+          category: null,
+        };
+      }
+    }
+    finally {
+      await this.prisma.$disconnect();
+    }
+  }
+
+  @Delete("/:id")
+  public async deleteCategory(id: number): Promise<CategoryDeletionResponse> {
+    try {
+      const deletedCategory = await this.prisma.category.delete({
+        where: {
+          id
+        }
+      });
+
+      if (deletedCategory === null) {
+        return {
+          category: null,
+          message: null,
+          error: {
+            code: "500",
+            message: "",
+            meta: null
+          }
+        }
+      }
+
+      return {
+        message: ``,
+        category: deletedCategory
+      }
+    }
+    catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        return {
+          message: null,
+          category: null,
+          error: {
+            code: error.code,
+            message: error.message,
+            meta: error.meta
+          }
+        };
+      } else {
+        return {
+          message: `Error deleting user with id ${id} Internal server error}`,
+          category: null,
+        };
+      }
+    }
+    finally {
+      await this.prisma.$disconnect();
+    }
+  }
+
+  public async CheckIfCategoryExists(Name: string): Promise<CheckCategoryAvailabilityResponse> {
+    try {
+      const category = await this.prisma.category.findMany({
+        where: {
+          Name: Name
+        }
+      })
+
+      return {
+        isAvailable: category.length === 0
+      }
+    }
+    catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        return {
+          isAvailable: null,
+          error: {
+            code: error.code,
+            message: error.message,
+            meta: error.meta
+          }
+        };
+      } else {
+        return {
+          isAvailable: null,
+          error: {
+            code: "500",
+            message: "Something went wrong checking for username availability",
+            meta: null,
+          }
+        };
+      }
+    }
+    finally {
+      await this.prisma.$disconnect();
     }
   }
 }
--- src/routes/CategoryRoute.ts
+++ src/routes/CategoryRoute.ts
@@ -1,40 +1,99 @@
 import { Router, type Request, type Response } from "express";
-import type { ValidationChain } from "express-validator";
-import { hashPassword } from "../utils";
+import { body, validationResult, type ValidationChain } from 'express-validator';
+import { createCategoryErrorMessages } from "../utils/validationErrors";
+import { RESPONSE_CODES } from "../types";
+import type { Category, NewCategory } from "../models/Category";
+import { CategoriesController } from "../controllers/CategoriesController";
 
 export const categoryRouter = Router();
 
-categoryRouter.get('', async (req: Request, res: Response) => {
-  
+const controller = new CategoriesController();
+
+categoryRouter.get('/', async (req: Request, res: Response) => {
+  // Check for existence of category
+  const { Category, CheckIfExists } = req.query;
+
+  if (CheckIfExists == 'true' && Category != '') {
+    const response = await controller.CheckIfCategoryExists(Category as string);
+
+    if (response.error != null) {
+      res.status(RESPONSE_CODES.INTERNAL_SERVER_ERROR).json(response);
+
+      return;
+    }
+
+    res.status(RESPONSE_CODES.OK).json(response);
+  }
+
+  console.log(Category, CheckIfExists);
+
+  const response = await controller.getCategories();
+
+  if (response.error != null) {
+    res.status(RESPONSE_CODES.INTERNAL_SERVER_ERROR).json(response);
+  }
+
+  res.status(RESPONSE_CODES.OK).json(response);
 });
 
-categoryRouter.get('', (req: Request, res: Response) => {
+categoryRouter.get('/:id', async (req: Request, res: Response) => {
+  const { id } = req.params;
 
+  const parsedId = Number.parseInt(id);
+
+  if (isNaN(parsedId) || !Number.isInteger(parsedId) || parsedId <= 0) {
+    res.status(RESPONSE_CODES.BAD_REQUEST).json({
+      error: `Invalid category id ${id}`
+    })
+  }
+
+  const response = await controller.getCategory(parsedId)
+
+  res.status(RESPONSE_CODES.OK).json(response);
 });
 
-// TODO: make const and define values
-let createCategoryValidationChain: ValidationChain[] = [
-
+const createCategoryValidationChain: ValidationChain[] = [
+  body('Name')
+    .notEmpty()
+    .withMessage(createCategoryErrorMessages.categoryNameRequired)
+    .isLength({ min: 3, max: 20 })
+    .withMessage(createCategoryErrorMessages.categoryLength)
 ]
 
-categoryRouter.post('', (req: Request, res: Response) => {
+categoryRouter.post('/', createCategoryValidationChain, async (req: Request, res: Response) => {
+  const errors = validationResult(req);
 
+  if (errors != null && !errors.isEmpty()) {
+    return res.status(RESPONSE_CODES.BAD_REQUEST).json(errors);
+  }
+
+  const { Name } = req.body;
+
+  const newCategory: NewCategory = {
+    Name
+  }
+
+  const result = await controller.createCategory(newCategory);
+
+  if (result.error != null) {
+    res.status(RESPONSE_CODES.INTERNAL_SERVER_ERROR).json(result);
+  }
+
+  res.status(RESPONSE_CODES.CREATED).json(result);
 });
 
-// TODO: make const and define values
-let updateCategoryValidationChain: ValidationChain[] = [
+categoryRouter.delete('/:id', async (req: Request, res: Response) => {
+  const { id } = req.params;
 
-]
+  const parsedId = Number.parseInt(id);
 
-categoryRouter.put('', (req: Request, res: Response) => {
+  if (isNaN(parsedId) || !Number.isInteger(parsedId) || parsedId <= 0) {
+    res.status(RESPONSE_CODES.BAD_REQUEST).json({
+      error: `Invalid category id ${id}`
+    })
+  }
 
-});
+  const response = await controller.deleteCategory(parsedId);
 
-// TODO: make const and define values
-let deleteCategoryValidationChain: ValidationChain[] = [
-
-]
-
-categoryRouter.delete('', (req: Request, res: Response) => {
-
+  res.status(RESPONSE_CODES.OK).json(response);
 });
