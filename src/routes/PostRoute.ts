023813fcb829835bc0f9cd3cf8a0e3505b10c7d5
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2024-04-25 03:17:47 -0600

    update(routes): PostRoute
    Implement post route with full controller features.

--- src/routes/PostRoute.ts
+++ src/routes/PostRoute.ts
@@ -1,40 +1,78 @@
 import { Router, type Request, type Response } from "express";
-import type { ValidationChain } from "express-validator";
+import { body, validationResult, type ValidationChain } from "express-validator";
+import { createPostErrorMessages } from "../utils/validationErrors";
+import { RESPONSE_CODES } from "../types";
+import type { NewPost } from "../models/Post";
+import { PostsController } from "../controllers/PostsController";
+import { isValidDate } from "../utils";
+import type { UserID } from "../models/User";
 
 export const postRouter = Router();
 
-postRouter.get('/', (req: Request, res: Response) => {
+const controller = new PostsController();
 
-}); 
+postRouter.get('/', async (req: Request, res: Response) => {
+
+});
 
 
-postRouter.get('/:id', (req: Request, res: Response) => {
+postRouter.get('/:id', async (req: Request, res: Response) => {
 
-}); 
+});
 
-// TODO: make const and define values
-let createPostValidationChain: ValidationChain[] = [
+const postValidationChain: ValidationChain[] = [
+  body('Title')
+    .notEmpty()
+    .withMessage(createPostErrorMessages.postTitleRequired)
+    .isLength({ min: 5, max: 40 })
+    .withMessage(createPostErrorMessages.postTitleLength),
 
+  body('Content')
+    .notEmpty()
+    .withMessage(createPostErrorMessages.postContentRequired),
+
+  body('CreationDate')
+    .notEmpty()
+    .withMessage(createPostErrorMessages.postDateRequired)
+    .custom((value) => !isValidDate(value))
+    .withMessage(createPostErrorMessages.postIsValidDate),
+
+  body('PostCategory')
+    .notEmpty()
+    .withMessage(createPostErrorMessages.postCategoryRequired)
 ]
 
-postRouter.post('/', (req: Request, res: Response) => {
+postRouter.post('/', postValidationChain, async (req: Request, res: Response) => {
+  const errors = validationResult(req);
 
-}); 
+  if (errors != null && !errors.isEmpty()) {
+    return res.status(RESPONSE_CODES.BAD_REQUEST).json(errors);
+  }
 
-// TODO: make const and define values
-let updatePostValidationChain: ValidationChain[] = [
+  const { Title, Content, CreationDate, UpdateDate, PostCategory, User } = req.body;
 
-]
+  const newPost: NewPost = {
+    Title,
+    Content,
+    CreationDate,
+    UpdateDate,
+    PostCategory,
+    User
+  }
 
-postRouter.post('/:id', (req: Request, res: Response) => {
+  const result = await controller.createPost(newPost);
 
-}); 
+  if (result.error != null) {
+    res.status(RESPONSE_CODES.INTERNAL_SERVER_ERROR).json(result);
+  }
 
-// TODO: make const and define values
-let deletePostValidationChain: ValidationChain[] = [
+  res.status(RESPONSE_CODES.CREATED).json(result);
+});
 
-]
+postRouter.put('/:id', postValidationChain, async (req: Request, res: Response) => {
 
-postRouter.delete('/:id', (req: Request, res: Response) => {
+});
+
+postRouter.delete('/:id', async (req: Request, res: Response) => {
 
 }); 
