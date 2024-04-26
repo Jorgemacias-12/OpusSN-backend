c9586e73af284ce2b0d36a1b0c4f97615b255771
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2024-04-26 15:39:29 -0600

    add(route&controllers): /comments endpoint

--- server.ts
+++ server.ts
@@ -3,7 +3,7 @@ import express, { type Express } from 'express';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import swaggerUi from 'swagger-ui-express';
-import { categoryRouter, postRouter, userRouter } from './src/routes';
+import { categoryRouter, postRouter, userRouter, commentRouter } from './src/routes';
 import swaggerDocument from './build/public/swagger.json'
 import { fileURLToPath } from 'bun';
 import { dirname, join } from 'path'
@@ -20,7 +20,6 @@ app.use(cors());
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(express.static("./build/public"));
 app.use(express.static(join(__dirname, 'build/public')));
 
 // Disable powered by 
@@ -29,6 +28,7 @@ app.disable('x-powered-by');
 app.use('/users', userRouter);
 app.use('/categories', categoryRouter);
 app.use('/posts', postRouter);
+app.use('/comments', commentRouter);
 
 app.use(
   "/docs",
---
+++ src/routes/CommentRoute.ts
@@ -0,0 +1,23 @@
+import { Router } from "express";
+
+export const commentRouter = Router();
+
+commentRouter.get('/', () => {
+
+});
+
+commentRouter.get('/id', () => {
+
+});
+
+commentRouter.post('/', () => {
+
+});
+
+commentRouter.put('/:id', () => {
+
+});
+
+commentRouter.delete('/:id', () => {
+
+});
